703ed2423a2df10bce5cc5a2d80cc2e85e41bd87
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-07-19 20:48:45 +0200

    Ein wenig schöner

--- GRDM_U6.java
+++ GRDM_U6.java
@@ -122,55 +122,55 @@ public class GRDM_U6 implements PlugInFilter {
 					
 					int pos = oldX + oldY * width;
 					
-                    //Pixel auslesen für A, B, C, D in Relation zu A
+                    			//Pixel auslesen für A, B, C, D in Relation zu A
 					//Einfachster Fall: alles Randpixel, daher = A
-                    A = pix[pos];
-                    B = pix[pos];
-                    C = pix[pos];
-                    D = pix[pos];
-                    
-                    //B, C, D mit Randbehandlung
-                    if (oldX != width-1) {
-                    	B = pix[pos+1];
-                    	if (oldY == height-1) {
-                    		D = pix[pos+1];
-                    	} else {
-                            D = pix[pos+1+width];
-                        }
-                    }
-                    
-                    else if (oldY != height-1) {
-                        C = pix[pos+width];
-                        if (oldX == width-1) {
-                        	D = pix[pos+width];
-                        } else {
-                            D = pix[pos+1+width];
-                        }
-                    }
-                    
-                    //RGB Werte von A, B, C, D holen
-                    int rA = (A >> 16) & 0xff;
-                    int gA = (A >> 8) & 0xff;
-                    int bA = A & 0xff;
-                    
-                    int rB = (B >> 16) & 0xff;
-                    int gB = (B >> 8) & 0xff;
-                    int bB = B & 0xff;
-                    
-                    int rC = (C >> 16) & 0xff;
-                    int gC = (C >> 8) & 0xff;
-                    int bC = C & 0xff;
-                    
-                    int rD = (D >> 16) & 0xff;
-                    int gD = (D >> 8) & 0xff;
-                    int bD = D & 0xff;
-                    
-                    //Formel anwenden
-                    int r = (int) (rA*(1-h)*(1-v) + rB*h*(1-v) + rC*(1-h)*v + rD*h*v);
-                    int g = (int) (gA*(1-h)*(1-v) + gB*h*(1-v) + gC*(1-h)*v + gD*h*v);
-                    int b = (int) (bA*(1-h)*(1-v) + bB*h*(1-v) + bC*(1-h)*v + bD*h*v);
-                    
-                    pix_n[pos_n] = (0xff<<24) | (r<<16) | (g<<8) | (b);
+					A = pix[pos];
+					B = pix[pos];
+					C = pix[pos];
+					D = pix[pos];
+
+					//B, C, D mit Randbehandlung
+					if (oldX != width-1) {
+						B = pix[pos+1];
+						if (oldY == height-1) {
+							D = pix[pos+1];
+						} else {
+							D = pix[pos+1+width];
+						}
+					    }
+
+					else if (oldY != height-1) {
+						C = pix[pos+width];
+						if (oldX == width-1) {
+							D = pix[pos+width];
+						} else {
+							D = pix[pos+1+width];
+						}
+					}
+
+					//RGB Werte von A, B, C, D holen
+					int rA = (A >> 16) & 0xff;
+					int gA = (A >> 8) & 0xff;
+					int bA = A & 0xff;
+
+					int rB = (B >> 16) & 0xff;
+					int gB = (B >> 8) & 0xff;
+					int bB = B & 0xff;
+
+					int rC = (C >> 16) & 0xff;
+					int gC = (C >> 8) & 0xff;
+					int bC = C & 0xff;
+
+					int rD = (D >> 16) & 0xff;
+					int gD = (D >> 8) & 0xff;
+					int bD = D & 0xff;
+
+					//Formel anwenden
+					int r = (int) (rA*(1-h)*(1-v) + rB*h*(1-v) + rC*(1-h)*v + rD*h*v);
+					int g = (int) (gA*(1-h)*(1-v) + gB*h*(1-v) + gC*(1-h)*v + gD*h*v);
+					int b = (int) (bA*(1-h)*(1-v) + bB*h*(1-v) + bC*(1-h)*v + bD*h*v);
+
+					pix_n[pos_n] = (0xff<<24) | (r<<16) | (g<<8) | (b);
 				}
 			}
 		}
